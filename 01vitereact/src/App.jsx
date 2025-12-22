184ed80be991561295f96404c886293a62ad0d28
AyushSahu143 <sahuayush134@gmail.com>
2025-12-22 10:32:33 +0530

    implemented counter using useState hook

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,41 @@
 import About from "./Ayush"
-function App() {
+import { useState  } from "react"
+import "./App.css"
+
+export default function App() {
+
+  let [counter, setCounter] = useState(15)
   
+  // let counter = 15
+
+  const addValue = () => {
+    // counter = counter + 1
+    if(counter < 20) {
+    setCounter(counter+1)
+    console.log("add", counter) 
+    }   
+  }
+
+  const removeValue = () => {
+    // counter = counter - 1
+    if(counter > 0) {
+    setCounter(counter-1)   
+    console.log("remove", counter) 
+    }
+  }
 
   return (
     <>
+    <span className="container">
     <About/>
-    <h1>AS is Cool</h1>
-    <p>As usual</p>
+    <h1>useState Hook {counter}</h1>
+    <h2>Counter value: {counter}</h2>
+    <button onClick={addValue}>Add button</button>
+    <br />
+    <button onClick={removeValue}>Reduce button</button>
+    <p>Footer: {counter}</p>
+    </span>
     </>
   )
 }
 
-export default App
